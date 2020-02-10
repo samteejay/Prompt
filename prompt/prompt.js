e8f357b83a36ed2691c6c41d53fdedb73fea023b
Samuel Tijani <omalitijani@gmail.com>
2020-02-10 14:56:33 +0100

    Init commit

---
+++ prompt/prompt.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>prompt</title>
+<script>
+var question = "Your species?";
+var defaultAnswer = "human";
+var spec = prompt(question, defaultAnswer);
+</script>
+</head>
+<body>
+
+</body>
+</html>
